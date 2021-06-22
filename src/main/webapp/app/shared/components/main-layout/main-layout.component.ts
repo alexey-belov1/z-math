2cedf07ae96fbea1606de3f72648796d5b614fd6
alexey-belov1 <veshousis1@gmail.com>
2021-06-23 00:17:30 +0500

    Рефакторинг стилей

--- src/main/webapp/app/shared/components/main-layout/main-layout.component.ts
+++ src/main/webapp/app/shared/components/main-layout/main-layout.component.ts
@@ -1,29 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {animate, style, transition, trigger} from '@angular/animations';
-import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
-  animations: [
-    trigger('routeAnimations', [
-      transition(':enter', [
-        style({ opacity: 0.2 }),
-        animate(350)
-      ])
-    ])
-  ]
 })
 export class MainLayoutComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  prepareRoute(outlet: RouterOutlet): void {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
 
 }
